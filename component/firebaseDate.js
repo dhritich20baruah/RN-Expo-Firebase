d8aaabf7484d3769327ccc551fcfe359d0aa1adc
Dhriti <dhriti.baruah20@gmail.com>
2025-04-07 23:20:20 +0530

    crud initialized

--- component/firebaseDate.js
+++ component/firebaseDate.js
@@ -1,26 +1,44 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    SafeAreaView,
-    Alert,
-    TextInput,
-    TouchableOpacity,
-  } from "react-native";
-  import { useNavigation } from "@react-navigation/native";
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Alert,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { db } from "../firebaseConfig";
+import {
+  collection,
+  addDoc,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
+import { auth } from "../firebaseConfig";
 
-  export default function FirebaseData(){
-    return(
-        <View style={styles.container}>
-            <Text>Database</Text>
-        </View>
-    )
-  }
+export default function FirebaseData() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  const auth = auth();
+  const user = auth.currentUser;
+  const todosCollection = collection(db, 'todos')
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
+  return (
+    <View style={styles.container}>
+      <Text>Database</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
